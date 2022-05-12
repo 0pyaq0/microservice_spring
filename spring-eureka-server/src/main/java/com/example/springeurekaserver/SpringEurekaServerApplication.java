e6c2926fa8a624e3605699e132383ec34877c866
심이진 <s2011@e-mirim.hs.kr>
2022-05-12 11:55:54 +0900

    📝 :: Eureka Server 활성화

--- spring-eureka-server/src/main/java/com/example/springeurekaserver/SpringEurekaServerApplication.java
+++ spring-eureka-server/src/main/java/com/example/springeurekaserver/SpringEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.example.springeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer // Enable eureka server
 @SpringBootApplication
 public class SpringEurekaServerApplication {
 
